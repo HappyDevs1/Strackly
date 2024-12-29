6d6a4541bf870ecf52179242e45c8db655e91189
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-29 17:15:30 +0200

    feat: :sparkles: implementing the create user and login controller for users

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import User from "../models/userModel";
+import bcrypt from "bcrypt";
+
+const registerUser = async (req: Request, res: Response) => {
+  const { name, email, phone, password } = req.body;
+  try {
+    if (!name || !email || phone || !password ) {
+      return res.status(406).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = new User({ name, email, phone, password: hashedPassword  });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "New user creates successfully", user: newUser })
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error});
+  }
+}
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    if (!req.body) {
+      return res.status(406).json({ message: "Fields cannot be empty" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
