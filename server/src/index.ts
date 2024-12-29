f5edd147e2bd7d727d508283bf75575467526199
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-29 15:33:59 +0200

    build: :building_construction: connecting the server to mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,23 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const mongo = process.env.MONGO_URL;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+mongoose.connect(mongo as string)
+.then(() => {
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+})
+.catch((error) => {
+  console.error("Error connecting to MongoDB: ", error);
+})
