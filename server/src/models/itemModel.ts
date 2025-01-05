7462601166aff13a64893e5bbbcd5d89f91e278b
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-05 15:12:32 +0200

    feat: :sparkles: adding the item schema to handle items

---
+++ server/src/models/itemModel.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IItem extends Document {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  transactions: Array<any>;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ItemSchema: Schema = new Schema({
+  name: { type: String, required: true, unique: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true, default: 0 },
+  password: { type: String, required: true },
+  transactions: { type: Array, default: []},
+  createdAt: { type: Date, default: Date.now},
+  updatedAt: { type: Date, default: Date.now}
+});
+
+const Item = mongoose.model<IItem>("Item", ItemSchema);
+
+export default Item;
