f9dda85607168ee2c4cf01084d908a05992f3179
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-05 18:05:50 +0200

    feat: :sparkles: adding transaction router to interact with the transaction controller

---
+++ server/src/routes/transactionRoute.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { Request, Response} from "express";
+import { createTransaction, getTransactions } from "../controllers/transactionController";
+
+const router = express.Router();
+
+router.post("/create", createTransaction);
+router.get("/transactions", getTransactions);
+router.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the transaction route");
+});
+router.get("*", (req: Request, res: Response) => {
+  res.status(404).send("Route not found");
+});
+
+export default router;
