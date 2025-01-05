d3cc65f841910237de45f81a3033327cca152e6c
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-05 18:48:43 +0200

    feat: :sparkles: implementing jwt authentication

---
+++ server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../models/userModel";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+export const authenticateMiddleware = async (req: AuthRequest, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded: any = jwt.verify(token, JWT_SECRET as string);
+    const user = await User.findById(decoded.id) || User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
