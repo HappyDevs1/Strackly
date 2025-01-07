b71f0279d4d760df24a8483a49ddfc02795416b1
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-07 16:24:48 +0200

    feat: :sparkles: removing the null redirection and replacing it with the barcode scanner component

--- client/mobile/navigation/AppNavigator.tsx
+++ client/mobile/navigation/AppNavigator.tsx
@@ -14,6 +14,7 @@ import Notifications from "../screens/Notifications";
 import Welcome from "../screens/Welcome";
 import RegisterUser from "../screens/RegisterUser";
 import LoginUser from "../screens/LoginUser";
+import BarcodeScanner from "../screens/BarcodeScanner";
 
 // Styled components using NativeWind
 const StyledText = styled(Text);
@@ -76,7 +77,7 @@ function BottomTabs() {
       {/* Floating Tab */}
       <Tab.Screen
         name="BarcodeScanner"
-        component={() => null} // Temporary null component
+        component={BarcodeScanner} // Redirect to barcode scanner component
         options={{
           tabBarButton: (props) => <FloatingTabButton {...props} />,
           tabBarLabel: () => null, // Hide the label
