16c2dd9c71fd7f5566f8203a676566f9bffd6de0
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-07 19:28:55 +0200

    feat: :sparkles: adding the welcome message to the home page and icon for user profile

--- client/mobile/screens/Home.tsx
+++ client/mobile/screens/Home.tsx
@@ -1,16 +1,24 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { styled } from 'nativewind';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 
 const Home = () => {
   return (
-    <StyledView>
-      <StyledText>HomeScreen</StyledText>
-    </StyledView>
-  )
-}
+    <SafeAreaView style={{ flex: 1, margin: 25 }}> {/* SafeAreaView from safe-area-context */}
+      <StyledView className="flex flex-row gap-6 items-center">
+        <Icon name="person-outline" size={25} color="black" />
+        <StyledView className="flex flex-col">
+          <StyledText className="text-xl font-bold">Hello,</StyledText>
+          <StyledText className="text-2xl font-bold">Happy Mahlangu</StyledText>
+        </StyledView>
+      </StyledView>
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
