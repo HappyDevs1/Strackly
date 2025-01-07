0504ba003b546d1c3dfa3c45d78271870e38d54d
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-07 11:24:27 +0200

    feat: :sparkles: adding the login element UI

--- client/mobile/screens/LoginUser.tsx
+++ client/mobile/screens/LoginUser.tsx
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { styled } from 'nativewind';
+import Header from '../components/Header';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
-const LoginUser = () => {
+const StyledView = styled(View);
+const StyledText = styled(Text);
+
+const LoginUser = ({ navigation }: any) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>LoginUser</Text>
-    </View>
-  )
-}
+    <StyledView className="flex-1 bg-gray-100 justify-center px-6">
+      {/* Header Section */}
+      <Header
+        title="Login to access your account."
+        subtitle="Effortlessly login, access your account, and enjoy seamless convenience."
+      />
 
-export default LoginUser
+      {/* Form Section */}
+      <StyledView className="bg-white p-6 rounded-lg shadow-md mt-6">
+        <Input
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Input
+          label="Password"
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry // Hides the password input
+        />
+        <Button title="Login" onPress={() => console.log('Login')} style="mt-4" />
+      </StyledView>
+
+      {/* Already Have An Account Section */}
+      <StyledView className="flex items-center justify-center mt-6">
+        <StyledText className="text-gray-500 mb-1">
+          Don't have an account yet?
+        </StyledText>
+        <TouchableOpacity onPress={() => console.log('Navigate to Register')}>
+          <StyledText onPress={() => navigation.navigate("RegisterUser")} className="text-blue-500 font-semibold">Register</StyledText>
+        </TouchableOpacity>
+      </StyledView>
+    </StyledView>
+  );
+};
+
+export default LoginUser;
