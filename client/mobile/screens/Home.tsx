88e56329bae26619ec92a1b52f9a794be94dea7d
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-08 09:45:48 +0200

    feat: :sparkles: more styling for the home UI

--- client/mobile/screens/Home.tsx
+++ client/mobile/screens/Home.tsx
@@ -1,6 +1,5 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { styled } from 'nativewind';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -9,15 +8,26 @@ const StyledText = styled(Text);
 
 const Home = () => {
   return (
-    <SafeAreaView style={{ flex: 1, margin: 25 }}> {/* SafeAreaView from safe-area-context */}
-      <StyledView className="flex flex-row gap-6 items-center">
-        <Icon name="person-outline" size={25} color="black" />
-        <StyledView className="flex flex-col">
-          <StyledText className="text-xl font-bold">Hello,</StyledText>
-          <StyledText className="text-2xl font-bold">Happy Mahlangu</StyledText>
+    <StyledView>
+      <StyledView className="bg-blue-500 px-6 pb-[40%] rounded-3xl">
+        {/* Header Row */}
+        <StyledView className="flex flex-row gap-4 items-center my-6">
+          <StyledView className="flex bg-white p-3 rounded-xl">
+            <Icon name="person-outline" size={21} color="black" />
+          </StyledView>
+          <StyledView className="flex flex-col">
+            <StyledText className="text-lg font-semibold">Hello,</StyledText>
+            <StyledText className="text-xl font-bold text-white">Happy Mahlangu</StyledText>
+          </StyledView>
+        </StyledView>
+
+        {/* Calendar Row */}
+        <StyledView className="flex flex-row items-center w-full px-4 py-3 rounded-xl bg-white">
+          <Icon name="calendar-number" size={21} color="tomato" />
+          <StyledText className="ml-2 font-semibold text-black">07 January 2025</StyledText>
         </StyledView>
       </StyledView>
-    </SafeAreaView>
+    </StyledView>
   );
 };
 
