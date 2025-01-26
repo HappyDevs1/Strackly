9cc52485b634ec1e9bf5106144ae394541e0d6fa
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-26 20:59:42 +0200

    build: :sparkles: implementing react routing to  the navbar

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './navbar/NavBar';
+import Home from './navbar/Home';
+import Products from './navbar/Products';
+import Analytics from './navbar/Analytics';
+import Notifications from './navbar/Notifications';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/analytics' element={<Analytics />} />
+        <Route path='/notifications' element={<Notifications />} />
+      </Routes>
     </div>
   )
 }
--- client/web/src/navbar/NavBar.tsx
+++ client/web/src/navbar/NavBar.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
     <div className='border-b-2 border-gray-300'>
       <div className='flex justify-between mx-10 my-6 text-lg'>
-        <div className='text-2xl cursor-pointer'>Strackly</div>
+        <div className='text-2xl cursor-pointer'>
+          <Link to="/home">Strackly</Link>
+        </div>
         <div>
           <ul className='flex gap-10 cursor-pointer'>
-            <li className='hover:text-blue-400'>Home</li>
-            <li className='hover:text-blue-400'>Products</li>
-            <li className='hover:text-blue-400'>Analytics</li>
-            <li className='hover:text-blue-400'>Notifications</li>
+            <li className='hover:text-blue-400'>
+              <Link to="/home">Home</Link>
+            </li>
+            <li className='hover:text-blue-400'>
+              <Link to="/products">Products</Link>
+            </li>
+            <li className='hover:text-blue-400'>
+              <Link to="/analytics">Analytics</Link>
+            </li>
+            <li className='hover:text-blue-400'>
+              <Link to="/notifications">Notifications</Link>
+            </li>
           </ul>
         </div>
       </div>
