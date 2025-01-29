72f0977fdaec46d787b5f219ce5515f627cb985d
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-01-29 23:02:01 +0200

    build: :building_construction: adding cors to the express server to control incoming requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import userRoutes from "./routes/userRoute";
 import itemRoutes from "./routes/itemRoute";
 import transactionRoutes from "./routes/transactionRoute";
@@ -11,7 +12,13 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 const mongo = process.env.MONGO_URL;
 
+// enabling CORS for some specific origins only.
+let corsOptions = {
+  origin : ['http://localhost:8081', 'exp://10.32.214.32:8081'],
+}
+
 app.use(express.json());
+app.use(cors(corsOptions))
 
 app.use("/api/user", userRoutes);
 app.use("/api/item", itemRoutes);
