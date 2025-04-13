c6019ffdced5d3c83397f593d88e86706462408a
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-04-13 15:14:38 +0200

    feat: :sparkles: adding missing types to the interface and adding the master field to determine the master account and lastly editing the allowed to sell to true to allow the master account to be able to sell by default

--- server/src/models/masterUserModel.ts
+++ server/src/models/masterUserModel.ts
@@ -5,8 +5,11 @@ export interface IUser extends Document {
   name: string;
   email: string;
   phone: string;
+  organisationName: string;
+  organisationId: string;
   password: string;
   allowedToSell: boolean;
+  role: string;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -19,6 +22,7 @@ const UserSchema: Schema = new Schema({
   organisationId: { type: String, required: true },
   password: { type: String, required: true },
   allowedToSell: { type: Boolean, required: true, default: true },
+  master: {type: Boolean, required: true, default: true},
   createdAt: { type: Date, default: Date.now},
   updatedAt: { type: Date, default: Date.now}
 });
