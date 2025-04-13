cec090829e820496652cbd188ff00595699d1c94
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-04-14 00:38:55 +0200

    refactor: :recycle: API refactoring, following best practices to make it easy to scale the API and increase query speed

---
+++ server/src/models/organisationModel.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, mongo, Schema } from "mongoose";
+
+export interface IOrganisation extends Document {
+  _id: string;
+  organisationName: string;
+  masterUser: mongoose.ObjectId;
+  employees: mongoose.ObjectId[];
+  products: mongoose.ObjectId[];
+  transations: mongoose.ObjectId[];
+  revenue: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const OrganisationSchema: Schema = new Schema({
+  organisationName: { type: String, required: true },
+  masterUser: { type: mongoose.Schema.Types.ObjectId, ref: "MasterUser" },
+  employees: [{ type: mongoose.Schema.Types.ObjectId, ref: "EmployeeUser"}],
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+  transaction: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }],
+  revenue: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now},
+  updatedAt: { type: Date, default: Date.now}
+});
+
+const Organisation = mongoose.model<IOrganisation>("Organisation", OrganisationSchema);
+
+export default Organisation;
