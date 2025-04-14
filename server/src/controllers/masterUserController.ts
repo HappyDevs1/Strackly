3addb42eaef83dd98a81e97beb82beede3d0c5d1
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-04-14 15:37:59 +0200

    feat: :sparkles: master user controller to handle the main account of the organisation

--- server/src/controllers/masterUserController.ts
+++ server/src/controllers/masterUserController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 import MasterUser from "../models/masterUserModel";
+import EmployeeUser from "../models/employeeUserModel";
+import Organisation from "../models/organisationModel";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
@@ -13,15 +16,13 @@ export const registerMasterUser = async (
   res: Response
 ): Promise<any> => {
   try {
-    const { name, email, phone, organisationName, organisationId, password } =
+    const { name, email, phone, password } =
       req.body;
 
     if (
       !name ||
       !email ||
       !phone ||
-      !organisationName ||
-      !organisationId ||
       !password
     ) {
       return res.status(406).json({ message: "All fields are required" });
@@ -33,24 +34,28 @@ export const registerMasterUser = async (
       return res.status(409).json({ message: "User already exists" });
     }
 
+    const existingMasterUser = await MasterUser.find();
+
+    if (existingMasterUser) {
+      return res.status(409).json({ message: "Cannot have more than one master user" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const newUser = new MasterUser({
+    const newMasterUser = new MasterUser({
       name,
       email,
       phone,
-      organisationName,
-      organisationId,
       password: hashedPassword,
     });
 
-    await newUser.save();
+    await newMasterUser.save();
 
     res
       .status(201)
       .json({
         message: "New master user created successfully",
-        masterUser: newUser,
+        masterUser: newMasterUser,
       });
   } catch (error) {
     res.status(500).json({ message: "Internal server error", error });
@@ -87,10 +92,85 @@ export const loginMasterUser = async (
       .status(200)
       .json({
         message: "Master user login successful",
-        loggedInUser: user,
+        loggedInMasterUser: user,
         loginToken: token,
       });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getMasterUser = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(406).json({ message: "ID cannot be empty" });
+    }
+
+    const user = await MasterUser.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "Master user not found" });
+    }
+
+    res.status(200).json({ foundMasterUser: user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+export const createEmployee = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { username, password } = req.body;
+    const { orgId } = req.params;
+
+    if (!username || !password) {
+      return res.status(406).json({ message: "All fields are required" });
+    }
+
+    if (!orgId) {
+      return res.status(406).json({ message: "Organisation ID cannot be empty" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(orgId)) 
+      return res.status(404).json({ msg: `No organisation with : ${orgId}` });
+
+    const existingEmployee = await EmployeeUser.findOne({ username });
+
+    if (existingEmployee) {
+      return res.status(409).json({ message: "Employee already exists" });
+    }
+
+    const existingOrg = await Organisation.findById(orgId);
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    if (!existingOrg) {
+      return res.status(404).json({ message: "Organisation not found" });
+    } else {
+      const newEmployee = new EmployeeUser({
+        username,
+        password: hashedPassword,
+      });
+
+      await Organisation.findByIdAndUpdate(orgId, {
+        $push: { employees: newEmployee._id },
+        $set: { updatedAt: new Date() },
+      })
+
+      await newEmployee.save();
+
+      res.status(201).json({
+        message: "New employee created successfully",
+        employee: newEmployee,
+      });
+    }
+
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+}
