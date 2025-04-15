ac5317acba0294e0935a26a11f4fb4836318d23b
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-04-15 02:19:29 +0200

    feat: :sparkles: adding the login route to call a controller function

--- server/src/routes/employeeUserRoute.ts
+++ server/src/routes/employeeUserRoute.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import express from "express";
-import { createEmployeeUser, getEmployeeUser } from "../controllers/employeeUserController";
+import { createEmployeeUser, getEmployeeUser, loginEmployeeUser } from "../controllers/employeeUserController";
 
 const router = express.Router();
 
 router.post("/create/:orgId", createEmployeeUser);
+router.post("/login",loginEmployeeUser);
 router.get("/get/:id", getEmployeeUser);
 router.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome to the employee user route" });
