8f934fdeaf9ff0d0c1906840e710359b4a6c3592
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-04-17 21:30:54 +0200

    perf: :zap: returning the whole response to ensure that it can be easily reused without any issues in the components

--- client/mobile/services/organisation.ts
+++ client/mobile/services/organisation.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { IP_ADDRESS } from "../utils/config";
 
-const API_BASE_URL = "http://localhost:3000/api/organisation";
+const API_BASE_URL = `http://${IP_ADDRESS}:3000/api/organisation`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -12,8 +13,8 @@ const api = axios.create({
 export const createOrganisation = async (orgData: any, masId: string) => {
   try {
     const response = await api.post(`/create/${masId}`, orgData);
-    console.log("Organisation created successfully:", response.data);
-    return response.data;
+    console.log("Organisation created successfully:", response);
+    return response;
   } catch (error) {
     console.error("Error creating organisation:", error);
     throw error;
@@ -23,11 +24,11 @@ export const createOrganisation = async (orgData: any, masId: string) => {
 export const getOrganisation = async (orgId: string) => {
   try {
     const response = await api.get(`/get/${orgId}`);
-    console.log("Fetched organisation successfully:", response.data);
+    console.log("Fetched organisation successfully:", response);
     if (response.status !== 200) {
       throw new Error(`Error fetching organisation: ${response.statusText}`);
     }
-    return response.data;
+    return response;
   } catch (error) {
     console.error("Error fetching organisation:", error);
     throw error;
