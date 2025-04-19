c0c8014ee9c4e7c4c877a7d370b60715c7952434
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-04-19 15:33:58 +0200

    fix: :bug: updating the baseURL by using the ip address stored in the config

--- client/mobile/services/item.ts
+++ client/mobile/services/item.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { IP_ADDRESS } from "../utils/config";
 
-const API_BASE_URL = "http://localhost:3000/api/item";
+const API_BASE_URL = `http://${IP_ADDRESS}:3000/api/item`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
